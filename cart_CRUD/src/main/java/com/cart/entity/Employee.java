dbd886a6d6f90a4c86673d3b302106ccc4a1a951
Khushbu Shaikh <Khushbu Shaikh@TCOGNITION>
2023-05-12 17:33:10 +0530

    addProject

---
+++ cart_CRUD/src/main/java/com/cart/entity/Employee.java
@@ -0,0 +1,16 @@
+package com.cart.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	private int id;
+	private String name;
+	
+	private String sds;
+	private String sdssdsd;
+	private String df;
+}
